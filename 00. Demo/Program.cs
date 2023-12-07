4c2193fc108954c2710142e4e4b2850bffac62d0
Denis Atanassov <134636754+sh0cklife@users.noreply.github.com>
2023-12-07 05:22:08 +0200

    Add project files.

---
+++ 00. Demo/Program.cs
@@ -0,0 +1,26 @@
+﻿try
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            
+            if (number < 0)
+            {
+                throw new ArgumentException("Invalid number.");
+            }
+
+            
+            double squareRoot = Math.Sqrt(number);
+            Console.WriteLine($"{squareRoot}");
+        }
+        catch (ArgumentException ex)
+        {
+            
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            
+            Console.WriteLine("Goodbye.");
+        }
+    
+
---
+++ 03. SumOfIntegers/Program.cs
@@ -0,0 +1,25 @@
+﻿List<string> stringNumbers = Console.ReadLine().Split().ToList();
+
+int sum = 0;
+foreach (string number in stringNumbers)
+{
+    try
+    {
+        int num = int.Parse(number);
+        sum += num;
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine($"The element '{number}' is in wrong format!");
+    }
+    catch(OverflowException)
+    {
+        Console.WriteLine($"The element '{number}' is out of range!");
+    }
+    finally
+    {
+        Console.WriteLine($"Element '{number}' processed - current sum: {sum}");
+    }
+    
+}
+Console.WriteLine($"The total sum of all integers is: {sum}");
